0508ab92aaf5e72fbf0fe58174e4bd95240b4bb3
ThanhMC1977 <134181070+ThanhMC1977@users.noreply.github.com>
2023-11-16 21:42:16 +0700

    Delete Final Project directory

--- Final Project/javascript/function.js
+++
@@ -1,6 +0,0 @@
-function performSearch() {
-
-    var searchTerm = document.getElementById('searchInput').value;
-
-    document.getElementById('searchResults').innerHTML = '<p>Search results for: ' + searchTerm + '</p>';
-}
